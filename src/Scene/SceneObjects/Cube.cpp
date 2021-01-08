86462b318808ad3cd50896f117d470bfeabc9d9f
Ivan Hernandez <hernandezivancs@gmail.com>
2021-01-07 20:34:03 -0800

    Updated Cube rotation calculation

--- src/Scene/SceneObjects/Cube.cpp
+++ src/Scene/SceneObjects/Cube.cpp
@@ -7,39 +7,71 @@ Cube::Cube(glm::vec3 p, float s, ofColor diffuse) {
 }
 
 bool Cube::intersect(const Ray &ray, glm::vec3 &point, glm::vec3 &normal) {
+	glm::mat4 Translate, Rotation;
+	glm::vec3 rdd, roo, invdir, s, k, ko, ka, t1, t2;
+
 	Ray rTemp = Ray(ray.p, ray.d);
 
 	// Apply Transformation
-	glm::mat4 m = glm::translate(glm::mat4(1.0), position);
-	glm::mat4 M = glm::rotate(m, glm::radians(angle), glm::vec3(-axisR.x, axisR.y, axisR.z));
-	glm::vec3 rdd = (glm::inverse(M) * glm::vec4(ray.d.x, ray.d.y, ray.d.z, 0.0));
-	glm::vec3 roo = (glm::inverse(M) * glm::vec4(ray.p.x, ray.p.y, ray.p.z, 1.0));
+	Translate = glm::translate(glm::mat4(1.0), position);
+	Rotation = glm::rotate(Translate, glm::radians(angle), glm::vec3(-axisR.x, axisR.y, axisR.z));
+
+	glm::vec3 objOrigin = (Rotation * glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
+	//if (angle != 0.0) {
+		rdd = (glm::inverse(Rotation) * glm::vec4(ray.d.x, ray.d.y, ray.d.z, 0.0));
+		roo = (glm::inverse(Rotation) * glm::vec4(ray.p.x, ray.p.y, ray.p.z, 1.0));
+	//}
+	//else {
+	//	rdd = (glm::inverse(Translate) * glm::vec4(ray.d.x, ray.d.y, ray.d.z, 0.0));
+	//	roo = (glm::inverse(Translate) * glm::vec4(ray.p.x, ray.p.y, ray.p.z, 1.0));
+	//}
 
 	// Calculate intersection
-	glm::vec3 invdir = glm::vec3(1 / rdd.x, 1 / rdd.y, 1 / rdd.z);
-	glm::vec3 s = glm::vec3((rdd.x < 0.0) ? 1.0 : -1.0, (rdd.y < 0.0) ? 1.0 : -1.0, (rdd.z < 0.0) ? 1.0 : -1.0);
-	glm::vec3 k = s * glm::vec3(side / 2, side / 2, side / 2);
+	invdir = glm::vec3(1 / rdd.x, 1 / rdd.y, 1 / rdd.z);
+	s = glm::vec3((rdd.x < 0.0) ? 1.0 : -1.0, (rdd.y < 0.0) ? 1.0 : -1.0, (rdd.z < 0.0) ? 1.0 : -1.0);
+	k = s * glm::vec3(side / 2, side / 2, side / 2);
 
-	glm::vec3 ko = glm::vec3(-roo.x + k.x, -roo.y + k.y, -roo.z + k.z);
-	glm::vec3 ka = glm::vec3(-roo.x - k.x, -roo.y - k.y, -roo.z - k.z);
+	ko = glm::vec3(-roo.x + k.x, -roo.y + k.y, -roo.z + k.z);
+	ka = glm::vec3(-roo.x - k.x, -roo.y - k.y, -roo.z - k.z);
 
-	glm::vec3 t1 = invdir * ko;
-	glm::vec3 t2 = invdir * ka;
+	t1 = invdir * ko;
+	t2 = invdir * ka;
 
 	// Calculate entering and exiting points
 	float tN = max(max(t1.x, t1.y), t1.z);
 	float tF = min(min(t2.x, t2.y), t2.z);
 
-	if (tN > tF || tF < 0.0 || isnan(tF)) 
+	float d = glm::distance(abs(roo), objOrigin) - side;
+	//glm::vec3 d = abs(roo) - sqrt(2.0f);
+	//float cubeDist = MIN(MAX(d.x, max(d.y, d.z)), 0.0) + length(max(d, 0.0f));
+
+	if (tN > tF || tF < 0.0 || isnan(tF) || tN < d)
 		return false;
+
+	//cout << "abs(roo) " <<objOrigin << "  //  new: " << d << "  //  D: " << d << endl;
 
 	// Point
 	point = rTemp.evalPoint(tN);
 
 	// Normal
 	normal = -glm::sign(rdd) * glm::step(glm::vec3(t1.y, t1.z, t1.x), t1) * glm::step(glm::vec3(t1.z, t1.x, t1.y), t1);
-	normal = (M * glm::vec4(normal.x, normal.y, normal.z, 0.0));	
-	
+
+	if (angle != 0.0) {
+		normal = (glm::inverse(Rotation) * glm::vec4(normal.x, normal.y, normal.z, 0.0));	
+	}
+	else {
+		normal = (Translate * glm::vec4(normal.x, normal.y, normal.z, 0.0));
+	}
+
+	if (t1.x > t1.y && t1.x > t1.z) 
+		normal = glm::vec3(Rotation[0].x * s.x, Rotation[0].y*s.x, Rotation[0].z*s.x);
+	else if (t1.y > t1.z) 
+		normal = glm::vec3(Rotation[1].x * s.y, Rotation[1].y*s.y, Rotation[1].z*s.y);
+	else
+		normal = glm::vec3(Rotation[2].x * s.z, Rotation[2].y*s.z, Rotation[2].z*s.z);
+	//cout << "Point: " << point << ", Normal: " << normal << endl;
+	//cout << "		tN: " << tN << ", tF: " << tF << ", t1: " << t1 << ", t2: " << t2 << endl;
+
 	return true;
 }
 
